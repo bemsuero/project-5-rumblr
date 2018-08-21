3aafdf5c0067ccbe5ac483962b02039ef012162b
Bemilton Suero <bemsuero@yahoo.com>
2018-08-21 17:41:28 -0400

    heroku again

--- server.rb
+++ server.rb
@@ -5,8 +5,8 @@ enable :sessions
 
 
 require "active_record"
-# set :database, "sqlite3:project5.sqlite3"
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+# set :database, "sqlite3:project5.sqlite3"
 # THIS IS A HEROKU SPECIFIC ERROR. you have to remember this somehow. or at least reference back to it.
 
 get '/' do
