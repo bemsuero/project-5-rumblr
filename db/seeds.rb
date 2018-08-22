3a7355318fff667ea3cd2bd833332dd697e81ab6
Bemilton Suero <bemsuero@yahoo.com>
2018-08-22 15:41:08 -0400

    worked on media queries

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
 require "faker"
 
-5.times do
-User.create(
-  name: Faker::Superhero.name,
-  email: Faker::Internet.email
+11.times do
+Post.create(
+  owner: "Spidey",
+  post_title: "Fantastic Title",
+  description: "Fantastic content with words and things.",
+  image_url: "https://botw-pd.s3.amazonaws.com/styles/logo-thumbnail/s3/0016/3327/brand.gif?itok=0egZGLny"
+)
+end
+
+20.times do
+Post.create(
+  owner: "MJParker",
+  post_title: "Fantastic Title",
+  description: "Fantastic content with words and things.",
+  image_url: "https://botw-pd.s3.amazonaws.com/styles/logo-thumbnail/s3/0016/3327/brand.gif?itok=0egZGLny"
 )
 end
 # User.all.destroy_all
